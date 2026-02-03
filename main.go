abc5b5761a332e81ca717ced217863a5fe6f9a94
Himanshu <himan7781@gmail.com>
2026-02-03 22:08:26 +0530

    added TTL

--- main.go
+++ main.go
@@ -4,12 +4,23 @@ import (
 	"database/sql"
 	"fmt"
 	"sync"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 type Cache struct {
 	data sync.Map
+}
+
+type CacheEntry struct {
+	value     []byte
+	expiresAt *time.Time
+}
+
+type StoreTTL struct {
+	db    *sql.DB
+	cache *Cache
 }
 
 type Store struct {
@@ -26,41 +37,38 @@ func (s *Store) Set(key string, value []byte) error {
 		return err
 	}
 
-	// update cache
-	s.cache.data.Store(key, value)
+	// update cache with CacheEntry (no expiry)
+	s.cache.data.Store(key, CacheEntry{value: value, expiresAt: nil})
 	return nil
 }
 
 func (s *Store) Get(key string) ([]byte, error) {
-	// start := time.Now()
 	// 1. Cache lookup
 	if val, ok := s.cache.data.Load(key); ok {
-		// elapsed := time.Since(start)
-		// fmt.Printf("{GET} key=%s source=blah blah time=%s\n", key, elapsed)
-		return val.([]byte), nil
+		ce := val.(CacheEntry)
+		// If expiresAt is set and expired, delete and return nil
+		if ce.expiresAt != nil && time.Now().After(*ce.expiresAt) {
+			s.cache.data.Delete(key)
+			s.db.Exec("DELETE FROM kv WHERE key = $1", key)
+			return nil, nil
+		}
+		return ce.value, nil
 	}
 
 	// 2. DB lookup
 	var val []byte
 	err := s.db.QueryRow("SELECT value FROM kv WHERE key = $1", key).Scan(&val)
 	if err == sql.ErrNoRows {
-		// elapsed := time.Since(start)
-		// fmt.Printf("{GET} key=%s source=db time=%s\n", key, elapsed)
 		return nil, nil
 	}
-
 	if err != nil {
 		return nil, err
 	}
 
-	// 3. Populate cache
-	s.cache.data.Store(key, val)
-
-	// elapsed := time.Since(start)
-	// fmt.Printf("{GET} key=%s source=db time=%s\n", key, elapsed)
+	// 3. Populate cache with CacheEntry (no expiry)
+	s.cache.data.Store(key, CacheEntry{value: val, expiresAt: nil})
 
 	return val, err
-
 }
 
 func (s *Store) Delete(key string) error {
@@ -73,26 +81,7 @@ func (s *Store) Delete(key string) error {
 	return err
 }
 
-func main() {
-	db, err := sql.Open("postgres", "postgres://kvuser:kvpass@localhost:5432/kvdb?sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-	store := &Store{db: db, cache: &Cache{}}
-
-	// store.Set("hello", []byte("world"))
-	// val, _ := store.Get("hello")
-	// log.Print(string(val))
-
-	// store.Set("Louis", []byte("bar"))
-
-	// v1, _ := store.Get("Louis") // first GET → DB
-	// log.Print(string(v1))
-	// v2, _ := store.Get("Louis") // first GET → DB
-	// log.Print(string(v2))
-	// v3, _ := store.Get("Louis") // first GET → DB
-	// log.Print(string(v3))
-
+func concurrentExecution(store Store) {
 	var wg sync.WaitGroup
 
 	workers := 10
@@ -129,5 +118,101 @@ func main() {
 
 	wg.Wait()
 	fmt.Println("Concurrrent test completed")
+}
 
+func (s *StoreTTL) SetWithTTL(key string, value []byte, ttlSeconds int64) error {
+	expiresAt := time.Now().Add(time.Duration(ttlSeconds) * time.Second)
+	_, err := s.db.Exec(
+		`INSERT INTO kv (key,value,expires_at)
+	       VALUES ($1,$2,$3)
+	       ON CONFLICT (key)
+	       DO UPDATE SET value = $2, expires_at = $3
+	       `, key, value, expiresAt,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	// update cache with expiry info
+	s.cache.data.Store(key, CacheEntry{
+		value:     value,
+		expiresAt: &expiresAt,
+	})
+
+	return nil
+}
+
+func (s *StoreTTL) GetTTL(key string) ([]byte, error) {
+	start := time.Now()
+
+	// 1. Cache check
+	if entry, ok := s.cache.data.Load(key); ok {
+		ce := entry.(CacheEntry)
+
+		if ce.expiresAt != nil && time.Now().After(*ce.expiresAt) {
+			// expired
+			s.cache.data.Delete(key)
+			s.db.Exec("DELETE FROM kv WHERE key = $1", key)
+
+			fmt.Printf("[GET] key=%s expired (cache)\n", key)
+
+			return nil, nil
+		}
+		fmt.Printf("[GET] key=%s source=cache time=%s\n", key, time.Since(start))
+		return ce.value, nil
+	}
+
+	// 2. DB lookup
+	var val []byte
+	var expiresAt *time.Time
+
+	err := s.db.QueryRow(
+		"SELECT value, expires_at FROM kv WHERE key = $1", key,
+	).Scan(&val, &expiresAt)
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	// 3. Expiry check
+	if expiresAt != nil && time.Now().After(*expiresAt) {
+		s.db.Exec("DELETE FROM kv WHERE key = $1", key)
+		fmt.Printf("[GET] key=%s expired {db}\n", key)
+	}
+
+	// 4. Populate cache
+	s.cache.data.Store(key, CacheEntry{
+		value:     val,
+		expiresAt: expiresAt,
+	})
+
+	fmt.Printf("[GET] key=%s source=db time=%s\n", key, time.Since(start))
+
+	return val, nil
+}
+
+func TTLCodeExecution(storeTTL *StoreTTL) {
+	storeTTL.SetWithTTL("temp", []byte("hello"), 2)
+
+	storeTTL.GetTTL("temp")
+	time.Sleep(3 * time.Second)
+	storeTTL.GetTTL("temp")
+}
+
+func main() {
+	db, err := sql.Open("postgres", "postgres://kvuser:kvpass@localhost:5432/kvdb?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	// store := &Store{db: db, cache: &Cache{}}
+
+	storeTTL := &StoreTTL{db: db, cache: &Cache{}}
+
+	// concurrentExecution(*store)
+	TTLCodeExecution(storeTTL)
 }
